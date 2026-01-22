d018d306af6a8c6344c2b3579b12f4ca1514ed2a
titanicprime <andredelima@gmail.com>
2026-01-22 09:06:15 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from db.connection import Base, engine
 from api.attestations import router as attest_router
+from api.users import router as users_router
 from models.attestation import AttestationModel
 
 Base.metadata.create_all(bind=engine)
@@ -8,3 +9,4 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(title="BitRep Attestations - Modular")
 
 app.include_router(attest_router)
+app.include_router(users_router)
