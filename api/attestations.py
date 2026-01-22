aa02546e63abe8b8c7ef2584e90e43b5e58dcc9a
titanicprime <andredelima@gmail.com>
2026-01-22 09:00:35 -0800

    Update attestations.py

--- api/attestations.py
+++ api/attestations.py
@@ -1,1 +1,31 @@
-# endpoints for attestations will go here
+# endpoints for attestations
+
+from fastapi import APIRouter
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from db.connection import SessionLocal
+from models.attestation import AttestationModel
+from models.user import AttestationOut, AttestationIn
+
+router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/attest", response_model=AttestationOut)
+def create_attestation(att: AttestationIn, db: Session = Depends(get_db)):
+    db_att = AttestationModel(
+        from_user=att.from_user,
+        to_user=att.to_user,
+        value=att.value,
+        context=att.context,
+    )
+    db.add(db_att)
+    db.commit()
+    db.refresh(db_att)
+    return db_att
+
