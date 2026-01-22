9ed43159a70f8916ce82b7849c5cdee38134f86a
titanicprime <andredelima@gmail.com>
2026-01-22 09:00:05 -0800

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,1 +1,22 @@
-# user model will go here
+# user model
+
+from pydantic import BaseModel
+from datetime import datetime
+from typing import List
+
+class AttestationOut(BaseModel):
+    id: int
+    from_user: str
+    to_user: str
+    value: int
+    context: str
+    timestamp: datetime
+
+    class Config:
+        orm_mode = True
+
+class UserReputation(BaseModel):
+    user: str
+    reputation: int
+    attestations: List[AttestationOut]
+
