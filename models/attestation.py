b369d44e6c66b132facfff6c29a68bda95c95959
titanicprime <andredelima@gmail.com>
2026-01-22 08:59:43 -0800

    Update attestation.py

--- models/attestation.py
+++ models/attestation.py
@@ -1,2 +1,16 @@
-# attestation model will go here
+# attestation model
+
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+from db.connection import Base
+
+class AttestationModel(Base):
+    __tablename__ = "attestations"
+
+    id = Column(Integer, primary_key=True, index=True)
+    from_user = Column(String, index=True)
+    to_user = Column(String, index=True)
+    value = Column(Integer)
+    context = Column(String)
+    timestamp = Column(DateTime, default=datetime.utcnow)
 
